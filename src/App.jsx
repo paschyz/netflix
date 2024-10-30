9de39508836364eeccd93a8f78b13585e69a58bc
paschyz <paschy2002@gmail.com>
2024-10-30 00:39:33 -0400

    profiles done

--- src/App.jsx
+++ src/App.jsx
@@ -29,18 +29,18 @@ function App() {
 
   ])
   return (
-    <>
-      <div className='flex-col w-full '>
+    <div className='flex-col w-full'>
+      <div className='flex-col'>
         <h1 className='text-[3.5vw] text-white text-center my-[.67em]'>Qui est-ce ?</h1>
         <ul className='flex gap-[2vw] justify-center'>
 
           {
             profiles.map((profile, index)=>{
               return(
-                <li>
+                <li className='hover:text-white text-[#808080] '>
                   <a href="">
-                    <div style={{backgroundImage: `url(${profile.url})`}} className='h-[10vw] w-[10vw] bg-cover rounded-[4px]'></div>
-                    <span className='text-white text-center block my-[.6em] text-[1.3vw] text-grey'>{profile.name}</span>
+                    <div style={{backgroundImage: `url(${profile.url})`}} className='h-[10vw] w-[10vw] bg-cover rounded-[4px] hover:outline-[3px] hover:outline '></div>
+                    <span className='text-center block my-[.6em] text-[1.3vw]  '>{profile.name}</span>
                   </a>
               </li>
               )
@@ -48,8 +48,13 @@ function App() {
           }
          
         </ul>
+        
+
       </div>
-    </>
+      <div className='flex justify-center text-[1.2vw] mt-[2em] mb-[1em]'>
+      <span className=' px-[1.5em] py-[0.5em] text-[#808080]  hover:text-white border border-[#808080] hover:border-white cursor-pointer'>Gérer les profils</span>
+      </div>
+    </div>
   )
 }
 
