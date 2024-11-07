86cc6331ddbabef405a5fee016cd0ba854c44208
paschyz <paschy2002@gmail.com>
2024-11-06 23:10:23 -0500

    header FINALLY done

--- src/Profiles.jsx
+++ src/Profiles.jsx
@@ -1,29 +1,8 @@
 import { useState } from "react";
-
+import { profiles } from "./utils";
 export default function Profiles() {
   const [count, setCount] = useState(0);
-  const [profiles, setProfiles] = useState([
-    {
-      name: "Kaiser",
-      url: "https://i.pinimg.com/736x/b3/1e/64/b31e64f86809038abde8a9a3936877c0.jpg",
-    },
-    {
-      name: "Isagi",
-      url: "https://i.pinimg.com/736x/cf/3a/e5/cf3ae59448715e00d395b34fb5abe869.jpg",
-    },
-    {
-      name: "Rin",
-      url: "https://i.pinimg.com/736x/ce/55/7f/ce557f0307ae8ab10c43409a06530475.jpg",
-    },
-    {
-      name: "Barou",
-      url: "https://i.pinimg.com/564x/8a/59/c8/8a59c8b4d5783035aeac74eaa12ccffe.jpg",
-    },
-    {
-      name: "Nagi",
-      url: "https://i.pinimg.com/736x/80/a3/a5/80a3a5338408831552eeca15ed8967d6.jpg",
-    },
-  ]);
+
   return (
     <div className="h-screen flex justify-center items-center">
       <div className="flex-col">
---
+++ src/utils.jsx
@@ -0,0 +1,22 @@
+export const profiles = [
+  {
+    name: "Kaiser",
+    url: "https://i.pinimg.com/736x/b3/1e/64/b31e64f86809038abde8a9a3936877c0.jpg",
+  },
+  {
+    name: "Isagi",
+    url: "https://i.pinimg.com/736x/cf/3a/e5/cf3ae59448715e00d395b34fb5abe869.jpg",
+  },
+  {
+    name: "Rin",
+    url: "https://i.pinimg.com/736x/ce/55/7f/ce557f0307ae8ab10c43409a06530475.jpg",
+  },
+  {
+    name: "Barou",
+    url: "https://i.pinimg.com/564x/8a/59/c8/8a59c8b4d5783035aeac74eaa12ccffe.jpg",
+  },
+  {
+    name: "Nagi",
+    url: "https://i.pinimg.com/736x/80/a3/a5/80a3a5338408831552eeca15ed8967d6.jpg",
+  },
+];
